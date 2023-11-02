fe8c4bb9abbf77a3e69094cc4f7fd5140365c114
valentecaio <cvalente.aio@gmail.com>
2023-11-02 12:59:32 -0300

    mesh: simplify code and add example

--- mesh/main.lua
+++ mesh/main.lua
@@ -11,8 +11,8 @@ local Utils = require("utils")
 
 -- the first line contains the number of vertices (n) and the number of faces (m).
 -- the following n lines contain the coordinates of the vertices.
--- the following m lines contain the faces.
--- each face is a list of 3 vertice indices
+-- the following m lines contain the faces, where each face is a list of 3 vertice indices
+-- since lua arrays start at 1, we add 1 to each index
 function parseInput(path)
   local lines = Utils.readFile(path)
   local n, m = lines[1]:match("(%d+) (%d+)")
@@ -28,11 +28,30 @@ function parseInput(path)
     table.insert(faces, {math.floor(a+1), math.floor(b+1), math.floor(c+1)})
   end
   return {
-    n = n,
-    m = m,
     vertices = vertices,
     faces = faces,
   }
+end
+
+-- generate output in the format:
+-- the first line contains the number of vertices (n) and the number of faces (m).
+-- the following n lines contain the coordinates of the vertices and the id of an adjacent face.
+-- the following m lines contain the faces. 
+-- each face is a list of 3 vertice indices followed by a list of 3 adjacent face indices.
+-- since lua arrays start at 1, we subtract 1 from each index
+function writeOutput(path, mesh)
+  local lines = {
+    #mesh.vertices .. " " .. #mesh.faces,
+  }
+  for _, v in ipairs(mesh.vertices) do
+    table.insert(lines, v.x .. " " .. v.y .. " " .. v.adj_face-1)
+  end
+  for _,f in ipairs(mesh.faces) do
+    -- -1 because lua arrays start at 1
+    table.insert(lines, f[1].vertex  -1 .. " " .. f[2].vertex  -1 .. " " .. f[3].vertex  -1
+              .. " " .. f[1].opp_face-1 .. " " .. f[2].opp_face-1 .. " " .. f[3].opp_face-1)
+  end
+  Utils.writeFile(path, lines)
 end
 
 -- find face that shares two vertices with the current face_id
@@ -46,47 +65,53 @@ function findOppositeFace(faces, face_id, vid1, vid2)
   return 0
 end
 
+function findAdjacentFace(faces, vertex_id)
+  for i, face in ipairs(faces) do
+    -- print("checking if vertex " .. vertex_id .. " appears in face " ..faceToString(face))
+    if Utils.listContains(face, vertex_id) then
+      return i
+    end
+  end
+end
+
 -- convert face to string for debugging
 function faceToString(face)
   return "Face{" .. face[1] .. ", " .. face[2] .. ", " .. face[3] .. "}"
 end
 
--- generate output in the format:
--- the first line is n and m.
--- the following n lines contain the coordinates of the vertices and the id of an adjacent face.
--- the following m lines contain the faces. 
--- each face is a list of 3 vertice indices followed by a list of 3 adjacent face indices.
-function writeOutput(path, mesh)
-  local lines = {
-    mesh.n .. " " .. mesh.m,
+
+----------- DUAL GRAPH FUNCTIONS -----------
+
+--[[ example:
+  mesh = {
+    vertices = {
+      {x = 0, y = 0, adj_face = 1}, -- x and y are the coordinates of the vertex
+      {x = 1, y = 0, adj_face = 2}, -- adj_face is the id of a random adjacent face
+      {x = 0, y = 1, adj_face = 2},
+    },
+    faces = {
+      {
+        {vertex = 1, opp_face = 2}, -- vertex is the id of the vertex in the vertices array
+        {vertex = 2, opp_face = 1}, -- opp_face is the id of the opposite face in the faces array
+        {vertex = 3, opp_face = 0}, -- or 0 if no opposite face exists
+      },
+      {
+        {vertex = 2, opp_face = 2},
+        {vertex = 3, opp_face = 2},
+        {vertex = 1, opp_face = 1},
+      },
+    },
   }
-  for _, v in ipairs(mesh.vertices) do
-    table.insert(lines, v.x .. " " .. v.y .. " " .. v.adj_face)
-  end
-  for _,f in ipairs(mesh.faces) do
-    -- -1 because lua arrays start at 1
-    table.insert(lines, f[1].vertex  -1 .. " " .. f[2].vertex  -1 .. " " .. f[3].vertex  -1
-              .. " " .. f[1].opp_face-1 .. " " .. f[2].opp_face-1 .. " " .. f[3].opp_face-1)
-  end
-  Utils.writeFile(path, lines)
-end
-
-
+]]--
 function createDualGraphMesh(input)
   -- VERTICES: for each vertex, find ANY adjacent face
   local vertices = {}
   for i, vertex in ipairs(input.vertices) do
-    for j, face in ipairs(input.faces) do
-      -- print("checking if vertex " .. i .. " appears in face " ..faceToString(face))
-      if Utils.listContains(face, i) then
-        table.insert(vertices, {
-          x = vertex.x,
-          y = vertex.y,
-          adj_face = j,
-        })
-        break
-      end
-    end
+    table.insert(vertices, {
+      x = vertex.x,
+      y = vertex.y,
+      adj_face = findAdjacentFace(input.faces, i)
+    })
   end
   -- Utils.printTable(input.vertices)
 
@@ -115,8 +140,6 @@ function createDualGraphMesh(input)
 
   -- final adj mesh
   return {
-    n = input.n,
-    m = input.m,
     vertices = vertices,
     faces = faces,
   }
